10efc1f0ad83bbeb9fc240cdcb5e8a2e66afd7ab
unknown <merz.sebastian95@gmail.com>
2023-04-02 19:21:46 +0200

    adding auto configuration updater

--- tapo_mqtt_bridge/adapter.py
+++ tapo_mqtt_bridge/adapter.py
@@ -7,93 +7,75 @@ import json
 import os
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import sys
+import subprocess
 from ruamel.yaml import YAML, representer
 from ruamel.yaml.compat import StringIO
 representer.RoundTripRepresenter.ignore_aliases = lambda x, y: True
 
 currentToken = ""
 hass_options = json.load(open('/data/options.json'))
-getCensoredToken = lambda token: (len(token) - 4) * "*" + token[len(token)-4:]
-log = lambda value: os.system(f'echo \'{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")} | {str(value).replace(currentToken, getCensoredToken(currentToken))}\'') if hass_options["logging"] else lambda:None 
+censorString = lambda token: (len(token) - 4) * "*" + token[len(token)-4:]
+log = lambda value: subprocess.run(f'echo "{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")} | {str(value).replace(currentToken, censorString(currentToken)).replace(hass_options["password"], censorString(hass_options["password"]))}"', shell=True) if hass_options["logging"] else lambda: None
 
 
+def update_yaml_file(yaml_file_path, update_value):
+    with open(yaml_file_path, "r") as yaml_file:
+        data = yaml.load(yaml_file)
+        initial_data = str(data)
 
+    # mqtt config
+    if "mqtt" not in data.keys():
+        data["mqtt"] = update_value["mqtt"]
+    else:
+        for key in update_value["mqtt"].keys():
+            for i, button in enumerate(update_value["mqtt"][key]):
+                if len([i for i in update_value["mqtt"][key] if i["unique_id"] == button["unique_id"]]) == 1:
+                    if key not in data["mqtt"].keys():
+                        data["mqtt"][key] = []
+                    index_map = [i for i, s in enumerate(data["mqtt"][key]) if s["unique_id"] == button["unique_id"]]
+                    if len(index_map) == 0:
+                        data["mqtt"][key].append(button)
+                    else:
+                        data["mqtt"][key][index_map[0]] = button
+    # camera config
+    if "camera" not in data.keys():
+        data["camera"] = update_value["camera"]
+    else:
+        for i, button in enumerate(update_value["camera"]):
+            if len([i for i in update_value["camera"] if i["name"] == button["name"]]) == 1:
+                index_map = [i for i, s in enumerate(data["camera"]) if s["name"] == button["name"]]
+                if len(index_map) == 0:
+                    data["camera"].append(button)
+                else:
+                    data["camera"][index_map[0]] = button
 
+    # Write the updated data back to the YAML file
+    with open(yaml_file_path, "w") as yaml_file:
+        yaml.dump(data, yaml_file)
 
+    return initial_data != str(data)
 
+# would be good to make a backup for the config
+log("Configuration editor | Checking if configuration.yaml needs update...")
+yaml = YAML()
+yaml.preserve_quotes = True
+update_value = yaml.load(open('/config_update.yaml'))
 
-config_update_data = YAML().load(open('/config_update.yaml'))
-log(str(config_update_data))
-log(type(config_update_data))
-
-
-# def update_yaml_file(yaml_file_path, update_value):
-#     with open(yaml_file_path, "r") as yaml_file:
-#         yaml = YAML()
-#         yaml.preserve_quotes = True
-#         data = yaml.load(yaml_file)
-#         initial_data = data
-        
-#     # mqtt config
-#     if "mqtt" not in data.keys():
-#         data["mqtt"] = update_value["mqtt"]
-#     else:
-#         for key in update_value["mqtt"].keys():
-#             for i, button in enumerate(update_value["mqtt"][key]):
-#                 if len([i for i in update_value["mqtt"][key] if i["unique_id"] == button["unique_id"]]) == 1:
-#                     if key not in data["mqtt"].keys():
-#                         data["mqtt"][key] = []
-#                     index_map = [i for i, s in enumerate(data["mqtt"][key]) if s["unique_id"] == button["unique_id"]]
-#                     if len(index_map) == 0:
-#                         data["mqtt"][key].append(button)
-#                     else:
-#                         data["mqtt"][key][index_map[0]] = button
-#     # camera config
-#     if "camera" not in data.keys():
-#         data["camera"] = update_value["camera"]
-#     else:
-#         for i, button in enumerate(update_value["camera"]):
-#             if len([i for i in update_value["camera"] if i["name"] == button["name"]]) == 1:
-#                 index_map = [i for i, s in enumerate(data["camera"]) if s["name"] == button["name"]]
-#                 if len(index_map) == 0:
-#                     data["camera"].append(button)
-#                 else:
-#                     data["camera"][index_map[0]] = button
-
-#     # Write the updated data back to the YAML file
-#     with open(yaml_file_path, "w") as yaml_file:
-#         yaml.dump(data, yaml_file)
-    
-#     return initial_data != data
-
-
-# # Auto create tapo entities in configuration.yaml - if they are not existing, they will get created. If the are existing, they will get updated. mqtt entries get identified based on the unique_id and the camera based on the name
-# changes = update_yaml_file('/config/configuration.yaml', {
-#     "mqtt": {
-#         "button": [
-#             { "unique_id": "tapo-cam_up", "name": "Tapo Cam - Move up", "command_topic": "tapo-cam/move/up", "payload_press": "10" },
-#             { "unique_id": "tapo-cam_down", "name": "Tapo Cam - Move down", "command_topic": "tapo-cam/move/down", "payload_press": "10" },
-#             { "unique_id": "tapo-cam_left", "name": "Tapo Cam - Move left", "command_topic": "tapo-cam/move/left", "payload_press": "10" },
-#             { "unique_id": "tapo-cam_right", "name": "Tapo Cam - Move right", "command_topic": "tapo-cam/move/right", "payload_press": "10" },
-#         ],
-#         "switch": [
-#             { "unique_id": "tapo-cam_privacy_switch", "name": "Tapo Cam - Privacy Switch", "state_topic": "tapo-cam/privacy", "command_topic": "tapo-cam/privacy/set", "payload_on": "ON", "payload_off": "OFF", "state_on": "ON", "state_off": "OFF" },
-#         ]
-#     },
-#     "camera": [{
-#         "platform": "ffmpeg",
-#         "name": "Tapo-C200",
-#         "input": f'-rtsp_transport tcp -i rtsp://{hass_options["username"]}:{hass_options["password"]}@{hass_options["ip"]}:554/stream1'
-#     }]
-# })
-
-# # Restart hass core (a bit shitty but works for now) BE CAREFUL SO THAT NO BOOTLOOP IS CREATED !!
-# os.system("printenv")
-# if changes or True:
-#     res = requests.post("http://supervisor/core/restart", headers={
-#         "Authorization": "Bearer " + os.environ.get('SUPERVISOR_TOKEN')
-#     })
-#     log("RESTART CORE " + res.text)
+# replace placeholder in update_Value
+update_value["camera"][0]["input"] = update_value["camera"][0]["input"].replace("<username>", hass_options["username"]).replace("<password>", hass_options["password"]).replace("<ip>", hass_options["ip"])
+log("Configuration editor | Checking for configuration.yaml updates and applying them if needed")
+changes = update_yaml_file('/config/configuration.yaml', update_value)
+log("Configuration editor | Where there any updates?: " + str(changes))
+if changes:
+    log("Configuration editor | Update finished! Restarting Home Assistant")
+    # Restart hass core (a bit shitty 'should' work for now), would be nice if i can only reload json somehow. Prob. possible. getting a 403 anyways
+    res = requests.post("http://supervisor/core/restart", headers={
+        "Authorization": "Bearer " + os.environ.get('SUPERVISOR_TOKEN')
+    })
+    if res.status_code != 200:
+        log("Configuration editor | ERROR => Reloading configuration.yaml resulted in the following response: " + res.text)
+    else:
+        log("Configuration editor | Good bye!")
 
 
 
@@ -106,6 +88,7 @@ log(type(config_update_data))
 
 #log(yaml.safe_load(open('/config/configuration.yaml')))
 
+# if threse is not mosquito installed or mqtt service available, this will throw an error, maybe use something descriptive to log here instead
 mqtt_response = requests.get("http://supervisor/services/mqtt", headers={
     "Authorization": "Bearer " + os.environ.get('SUPERVISOR_TOKEN')
 }).json()["data"]
@@ -142,9 +125,9 @@ def refresh_token():
     if res.status_code == 200:
         currentToken = res.json()["result"]["stok"]
 
-    
+
     log(f'Refresh Token => Status code: { str(res.status_code) }, response text: { res.text }' )
-    
+
 
 # def privacy_get():
 #    global headers
@@ -170,7 +153,6 @@ def privacy_set(status):
         verify=False
     )
 
-
 def move(direction, steps):
     global headers
     global url
@@ -190,25 +172,25 @@ def move(direction, steps):
     )
 
 def publish(client, topic, payload):
-    log(f'MQTT Publish => Topic: { topic }, Payload: { payload }')
+    log(f'MQTT | Publish => Topic: { topic }, Payload: { payload }')
     client.publish(topic, payload)
 
 def on_message(client, userdata, message):
     payload = str(message.payload.decode("utf-8")).lower()
-    log(f'MQTT Message => Incoming => topic: { message.topic }, payload: { payload }')
+    log(f'MQTT | Message => Incoming => topic: { message.topic }, payload: { payload }')
 
     # Privacy
     if message.topic == hass_options["mqtt_client_id"] + "/privacy/set" and (payload == "on" or payload == "off"):
         if privacy_set(payload).json()["error_code"] == 0:
             publish(client, f'{ hass_options["mqtt_client_id"] }/privacy', payload.upper())
-    
+
     # Move
     valid_topics = [f"{hass_options['mqtt_client_id']}/move/{direction}" for direction in ["right", "left", "up", "down"]]
     if message.topic in valid_topics:
         move(message.topic.split("/")[-1], payload)
 
 def subscribe(client, topic):
-    log(f'MQTT Subscribing => Topic: { topic }')
+    log(f'MQTT | Subscribing => Topic: { topic }')
     client.subscribe(topic)
 
 def on_connect(client, userdata, flags, rc):
